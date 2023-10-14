d7eb91395a09c46d54d8e8062ee9a0a967fc40b7
Semyon Vitsyn <89354991+SemyonVitsyn@users.noreply.github.com>
2023-10-15 02:34:08 +0300

    Add files via upload

---
+++ Basic|Sort|Structures/A-change/cmake-build-debug/CMakeFiles/A_change.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A_change.exe"
+  "A_change.exe.manifest"
+  "A_change.pdb"
+  "CMakeFiles/A_change.dir/main.c.obj"
+  "libA_change.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A_change.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
